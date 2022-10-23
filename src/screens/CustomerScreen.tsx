4e6a2369768e538193ce176b1b716b5c70f00bf1
cogentx <code@cogentx.com>
2022-10-23 17:04:20 -0230

    Build Customer Screen (2/3) - add image

--- src/screens/CustomerScreen.tsx
+++ src/screens/CustomerScreen.tsx
@@ -1,14 +1,37 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
 import { useTailwind } from 'tailwind-rn/dist';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { RootStackParamList } from '../navigator/RootNavigator';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Image } from '@rneui/themed';
+
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, 'Customers'>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const CustomerScreen = () => {
   const tw = useTailwind();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <SafeAreaView style={tw("flex-1 items-center justify-center")}>
-      <Text style={tw("text-blue-500")}>Legato</Text>
-    </SafeAreaView>
+    <ScrollView>
+      <Image
+        source={{
+          uri: 'https://i.imgur.com/uU8GTZM.jpeg',
+        }}
+        containerStyle={tw('w-full h-64')}
+      />
+    </ScrollView>
   );
 };
 
