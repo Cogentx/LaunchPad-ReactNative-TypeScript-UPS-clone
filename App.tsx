971922b67610c87ddba2df6d9c7b864c2e62f271
cogentx <code@cogentx.com>
2022-10-23 15:36:27 -0230

    Setup Root Navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React from 'react';
 import { TailwindProvider } from 'tailwind-rn';
+import { NavigationContainer } from '@react-navigation/native';
 import utilities from './tailwind.json';
 import CustomerScreen from './src/screens/CustomerScreen';
 
@@ -8,7 +8,9 @@ export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing type definitions
     <TailwindProvider utilities={utilities}>
-      <CustomerScreen />
+      <NavigationContainer>
+        <CustomerScreen />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
