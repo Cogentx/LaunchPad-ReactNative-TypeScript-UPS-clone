0012e773afec58f121633d41ac9103fab1d9204b
cogentx <code@cogentx.com>
2022-10-23 19:53:36 -0230

    Build Customer Screen (2/3) - add search bar

--- src/screens/CustomerScreen.tsx
+++ src/screens/CustomerScreen.tsx
@@ -1,12 +1,12 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+import { ScrollView, ActivityIndicator } from 'react-native';
 import { useTailwind } from 'tailwind-rn/dist';
 import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { TabStackParamList } from '../navigator/TabNavigator';
 import { RootStackParamList } from '../navigator/RootNavigator';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Image } from '@rneui/themed';
+import { Image, Input } from '@rneui/themed';
 
 export type CustomerScreenNavigationProp = CompositeNavigationProp<
   BottomTabNavigationProp<TabStackParamList, 'Customers'>,
@@ -15,7 +15,8 @@ export type CustomerScreenNavigationProp = CompositeNavigationProp<
 
 const CustomerScreen = () => {
   const tw = useTailwind();
-  const navigation = useNavigation();
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
+  const [input, setInput] = useState<string>('');
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -24,12 +25,20 @@ const CustomerScreen = () => {
   }, []);
 
   return (
-    <ScrollView>
+    <ScrollView style={{ backgroundColor: '#59C1CC' }}>
       <Image
         source={{
           uri: 'https://i.imgur.com/uU8GTZM.jpeg',
         }}
         containerStyle={tw('w-full h-64')}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+
+      <Input
+        placeholder="Search by Customer"
+        value={input}
+        onChangeText={setInput}
+        containerStyle={tw('bg-white pt-5 pb-0 px-10')}
       />
     </ScrollView>
   );
