5256e5d6d43d5d0efe4811573bf5109af8ce3291
cogentx <code@cogentx.com>
2022-10-23 16:33:05 -0230

    Add Stack and Tab Navigation

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { TailwindProvider } from 'tailwind-rn';
 import { NavigationContainer } from '@react-navigation/native';
 import utilities from './tailwind.json';
-import CustomerScreen from './src/screens/CustomerScreen';
+import RootNavigator from './src/navigator/RootNavigator';
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing type definitions
     <TailwindProvider utilities={utilities}>
       <NavigationContainer>
-        <CustomerScreen />
+        <RootNavigator />
       </NavigationContainer>
     </TailwindProvider>
   );
--- src/screens/OrderScreen.tsx
+++ src/screens/OrderScreen.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTailwind } from 'tailwind-rn/dist';
+
+const tw = useTailwind();
 
 const OrderScreen = () => {
   return (
-    <View>
+    <SafeAreaView style={tw('text-red-500')}>
       <Text>OrderScreen</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default OrderScreen
+export default OrderScreen;
